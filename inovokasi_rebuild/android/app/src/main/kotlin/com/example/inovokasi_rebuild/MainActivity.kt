7061e6b7322f69cd43d08558e7e7a6d773d46e28
fadiaskeyn <fadiaskeyn@gmail.com>
2025-01-07 10:38:39 +0700

    init: flutter

--- inovokasi_rebuild/android/app/src/main/kotlin/com/example/inovokasi_rebuild/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.inovokasi_rebuild
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- inovokasi_rebuild/lib/repositories/auth_repository.dart
+++
@@ -1,6 +0,0 @@
-class AuthRepository {
-  Future<bool> authenticate(String email, String password) async {
-    await Future.delayed(Duration(seconds: 2));
-    return email == 'admin@gmail.com' && password == 'admin123';
-  }
-}
